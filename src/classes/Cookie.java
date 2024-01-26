7de06c7f1fc24ef75e34073ac7cc660c4b04974b
tom474 <emsignouc0407@gmail.com>
2024-01-26 15:45:30 +0700

    upload code

---
+++ src/classes/Cookie.java
@@ -0,0 +1,17 @@
+package classes;
+
+public class Cookie {
+    private String color;
+
+    public Cookie(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
